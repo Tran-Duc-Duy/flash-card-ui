40a313e4b0d15766c6f432967f79d179ecb549bd
TranDucDuy_ЧанДыкЗюи <tranduybthv@gmail.com>
2022-07-26 00:56:01 +0300

    #8 - handle router and create CardList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import AddCard from './components/AddCard';
+import CardList from './components/CardList';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <AddCard />  
-    </> 
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<CardList />} />
+          <Route path="/" element={<CardList />} ></Route>
+          <Route path="/card-list" element={<CardList />} />
+          <Route path="/add-card" element={<AddCard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
